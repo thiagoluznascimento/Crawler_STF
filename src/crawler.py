18b9051e8cd6da78e2a882a8df71664f15e8fc7e
Thiago Luz Nascimento <thiago.nascimento@justicafacil.com.br>
2023-05-17 19:24:44 -0300

    Primeira versao completa de refatoracao O codigo precisa de uma revisão.

--- src/crawler.py
+++ src/crawler.py
@@ -1,4 +1,3 @@
-import sys
 import hashlib
 import os
 
@@ -29,20 +28,20 @@ class CrawlerStf:
         pagina_resultado_busca = self._busca_cadernos()
         links_cadernos = self._parser_links_cadernos(pagina_resultado_busca)
         links_pdfs = self._obtem_links_dj(links_cadernos)
-        slugs_pdfs = self._baixa_arquivos_pdf(links_pdfs)
-        #arquivos_pdf = self.slugs_pdfs)
-        #self._salva_arquivos(arquivos_pdf)
-        #self._exibe_conteudo_baixado(arquivos_pdf)
+        baixa_pdfs = self._baixa_arquivos_pdf(links_pdfs)
+        hashs_md5 = self._obtem_md5(baixa_pdfs)
 
 # Métodos utilitários
     def _busca_cadernos(self):
-        """Faz requisição no tribunal e retorna resultado"""
+        """Faz requisição no tribunal e retorna resultado
+        """
         link = self.URL_BUSCA.format(data=self._data_busca)
         resposta = requests.get(link, headers=self.HEADERS)
         return resposta.text
 
     def _parser_links_cadernos(self, pagina_resultado_busca):
-        '''Obtem os links dos cadernos do tribunal'''
+        '''Obtem os links dos cadernos do tribunal
+        '''
         soup = BeautifulSoup(pagina_resultado_busca, "html.parser")
         ul = soup.find("ul", {"class": "result__container--simples"})
         lista_anchor = ul.find_all('a')
@@ -52,7 +51,8 @@ class CrawlerStf:
         return lista_slugs
 
     def _obtem_links_dj(self, links_cadernos):
-        """Parsea os links que contém DJs do dia"""
+        """Parsea os links que contém DJs do dia
+        """
         links = links_cadernos
         listas_slugs_pdfs = []
         for link in links:
@@ -71,99 +71,23 @@ class CrawlerStf:
         return ancor_pdf['href']
 
     def _baixa_arquivos_pdf(self, links_pdfs):
-        """Itera os slugs dos pdfs e baixa faz requisição para obter o conteúdo
-        do pdfe retorna dicionario relacionando seu mdc com o seu conteudo."""
-        slugs_pdfs = links_pdfs
-        for slug in slugs_pdfs:
-            response = requests.get(url=slug, headers=self.HEADERS)
-            if response.status_code == 200:
-                with open(self._data_busca + "/" + self.hash_md5 + ".pdf", "wb") as f:
-                    f.write(slugs_pdfs.content)
-                print("Download concluído com sucesso.")
-            else:
-                print("Falha ao baixar o arquivo.")
-            if not os.path.exists(self._data_busca):
-                os.mkdir(self._data_busca)
-            dicionario[self._obtem_md5(response.content)] = response.content
+        """Verifica se existe a pasta com nome igual ao dia do arquivo e o cria se não existir;
+        itera os slugs dos pdfs e baixa, faz requisição para obter o conteúdodo pdf.
+        """
+        if not os.path.exists(self._data_busca):
+            os.mkdir(self._data_busca)
+        url = links_pdfs
+        self.nome_arquivo = 'teste.pdf'
+        response = requests.get(url, headers=self.HEADERS)
+        with open(self.nome_arquivo, "wb") as file:
+            file.write(response.content)
+        return response.content
 
-    def _obtem_md5(self, conteudo_pdf):
-        """recebe conteúdo pdf e calcula o hashMd5"""
-        self.hash_md5 = hashlib.md5(url.encode()).hexdigest()
-
-
-#############################################################################################################
-# Codigo antigo
-"""""
-def main():
-    # Verifica se foi passada uma data como argumento
-    if len(sys.argv) < 2:
-        print("Por favor, informe uma data no formato YYYY-MM-DD")
-        return
-
-    # Obtém a data informada
-    data_busca = sys.argv[1]
-
-    #Formata para dd/mm/yyyy
-    md_data = data_busca.split("-")
-    data_formatada = md_data[2] + "/" + md_data[1] + "/" + md_data[0]
-
-    # Definine informações do cabeçalho de soliticação
-    user_agent = {
-        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-        }
-
-    # URL de busca de arquivos referentes à data passada por parâmetro
-    url_busca_djs = f"https://portal.stf.jus.br/servicos/dje/listarDiarioJustica.asp?tipoVisualizaDJ=periodoDJ&txtNumeroDJ=&txtAnoDJ={md_data[0]}&dataInicial={data_busca}&dataFinal={data_busca}&tipoPesquisaDJ=&argumento="
-
-    # Retorna página com todos os DJs referentes à data
-    resposta = requests.get(url_busca_djs, headers=user_agent)
-    # Cria o objeto BeautifulSoup
-    soup = BeautifulSoup(resposta.content, "html.parser")
-    # Busca o ul com lista dos DJs
-    ul = soup.find("ul", {"class": "result__container--simples"})
-    # Cria HTML da variável ul
-    html = BeautifulSoup(ul.prettify(), "html.parser")
-    # Busca todas as tags a existentes na variável html
-    links = html.find_all("a")
-
-    for link in links:
-        # Divifine o conteúdo do href
-        split_link = link.get("href").split("&")
-        # Difine o conteúdo da variável split_link
-        split_dj = split_link[1].split("=")
-        # Atribui à variável a posição 1 do array split_dj que é referente ao número DJ
-        num_dj = split_dj[1]
-
-        if (num_dj != "0"):
-            # Link para a busca do PDF, de acordo com o numero do DJ e a data, passados por parâmetro
-            url = f"https://portal.stf.jus.br/servicos/dje/verDiarioEletronico.asp?numero={num_dj}&data={data_formatada}"
-
-            # Retorna página com o PDF
-            html = requests.get(url, headers=user_agent)
-            # Atribui à variável status o valor do status code
-            status = html.status_code
-
-            # Verifica se o status code é diferente de 200
-            if status != 200:
-                print("Não foi possível obter o arquivo PDF do Diário Oficial da União do STF")
-                return
-
-            # Cria o hash MD5 do arquivo PDF  ...  passei como encode() pq a funcao hashlib.md5() espera um obj tipo bytes
-            hash_md5 = hashlib.md5(url.encode()).hexdigest()
-
-            # Verifica se existe a pasta com nome igual ao dia do arquivo e o cria se não existir
-            if not os.path.exists(data_busca):
-                os.mkdir(data_busca)
-
-            # Abre e salva o arquivo PDF com o nome sendo o valor do seu respectivo hash
-            with open(data_busca + "/" + hash_md5 + ".pdf", "wb") as f:
-                f.write(html.content)
-
-            # Imprime o hash MD5 na saída padrão
-            print("No. DJ: " + num_dj + "\nHash: " + hash_md5 + "\n")
-
-        
-# verificando se a variavel __name__ == __main__,(verifica se este aquivo está sendo execultado) entao execulta o main() lá de cima.
-if __name__ == "__main__":
-    main()
-"""
+    def _obtem_md5(self, baixa_pdfs):
+        """recebe conteúdo pdf e calcula o hashMd5
+        """
+        response = requests.get(url=baixa_pdfs, headers=self.HEADERS)
+        pdf_content = response.content
+        md5_hash = hashlib.md5(pdf_content).hexdigest()
+        self.dicionario[md5_hash] = baixa_pdfs
+        return self.dicionario
