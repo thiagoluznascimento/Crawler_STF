cc1a97a9b5a6a312973a464c184901fceeb60216
Thiago Luz Nascimento <thiago.nascimento@justicafacil.com.br>
2023-05-23 20:40:10 -0300

    Alterações com melhorias que o Daniel solicitou!

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -18,15 +19,21 @@ class CrawlerStf:
         "periodoDJ&txtNumeroDJ=&txtAnoDJ=2022&dataInicial={data}&dataFinal={data}"
         "&tipoPesquisaDJ=&argumento="
     )
+
 # Método construtor
 
     def __init__(self, data):
         self._data_busca = data
+        if data > '2022-12-31':
+            print('O valor da data informada deve ser do dia "31-12-2022" ou anterior.')
+            sys.exit()
 
 # Chamando os métodos utilitarios
     def baixa_cadernos(self):
         pagina_resultado_busca = self._busca_cadernos()
         links_cadernos = self._parser_links_cadernos(pagina_resultado_busca)
+        if not links_cadernos:
+            return
         links_pdfs = self._obtem_links_dj(links_cadernos)
         self._baixa_arquivos_pdf(links_pdfs)
 
@@ -42,13 +49,12 @@ class CrawlerStf:
         '''Obtem os links dos cadernos do tribunal
         '''
         soup = BeautifulSoup(pagina_resultado_busca, "html.parser")
-        ul = soup.find("ul", {"class": "result__container--simples"})
-        if not ul:
+        section = soup.find("section", {"id": "conteudo"})
+        lista_anchor = section.find_all('a')
+        if not lista_anchor:
+            print('Não existem diarios na data informada.')
             return []
-        lista_anchor = ul.find_all('a')
         lista_slugs = [anchor['href'] for anchor in lista_anchor]
-        # for anchor in lista_anchor:
-        #     lista_slugs.append(anchor['href'])
         return lista_slugs
 
     def _obtem_links_dj(self, links_cadernos):
@@ -75,20 +81,21 @@ class CrawlerStf:
         """Verifica se existe a pasta com nome igual ao dia do arquivo e o cria se não existir;
         itera os slugs dos pdfs e baixa, faz requisição para obter o conteúdodo pdf.
         """
-        if not os.path.exists(self._data_busca):
-            os.mkdir(self._data_busca)
-
+        caminho = (r'/home/thiago/justica_facil/estagio/cadernos/ano/mes/dia')
+        os.makedirs(caminho, exist_ok=True)
         for link in links_pdfs:
             url = r'https://portal.stf.jus.br' + link
             response = requests.get(url, headers=self.HEADERS)
             nome_arquivo = self._obtem_md5(response.content) + ".pdf"
-            #import pdb; pdb.set_trace()
-            with open(self._data_busca + "/" + nome_arquivo, "wb") as file:
-                file.write(response.content)
+            if not os.path.exists(caminho + "/" + nome_arquivo):
+                with open(caminho + "/" + nome_arquivo, "wb") as file:
+                    file.write(response.content)
+                print(f'Caderno com o "{nome_arquivo}" salvo com sucesso!')
+            else:
+                print(f'Caderno com o "{nome_arquivo}" já existe!')
 
     def _obtem_md5(self, conteudo_pdf):
         """recebe conteúdo pdf e calcula o hashMd5
         """
         md5_hash = hashlib.md5(conteudo_pdf).hexdigest()
-        print(md5_hash)
         return md5_hash
